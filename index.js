cd69314fd4fe18ad90bad9cb18e2ff4e83b23359
UTKARSH GUPTA <67434242+utkarshgupta22@users.noreply.github.com>
2021-06-03 18:55:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// alert("Welcome to the DICEE 🎲")
+
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+let randomDiceImage = "dice" + randomNumber1 + ".png";
+
+let randomImageSource = "images/" + randomDiceImage;
+
+let image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent=" 🏆Dice 1 Wins!"
+}
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").textContent=" Dice 2 Wins! 🏆";
+}
+else{
+    document.querySelector("h1").textContent=" Draw 😬";
+}
